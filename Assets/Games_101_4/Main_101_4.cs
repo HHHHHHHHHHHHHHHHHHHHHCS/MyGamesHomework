a9f6041eeb3625d1aec11276304ef6d5d71cb8f6
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-04-26 20:20:00 +0800

    add

--- Assets/Games_101_4/Main_101_4.cs
+++ Assets/Games_101_4/Main_101_4.cs
@@ -9,31 +9,41 @@ namespace Games_101_4
 {
 	public class Main_101_4 : MonoBehaviour
 	{
-		private const string k_CSMain = "CSMain";
+		private const string k_CalcPoints = "CalcPoints";
+		private const string k_BlitScreen = "BlitScreen";
 
-		private static readonly int BlitTex_ID = Shader.PropertyToID("_BlitTex");
 		private static readonly int Result_ID = Shader.PropertyToID("_ResultTex");
 		private static readonly int Width_ID = Shader.PropertyToID("_Width");
 		private static readonly int Height_ID = Shader.PropertyToID("_Height");
 
 
 		public Transform pointsParent;
-
+		public int tCount = 101;
 		public ComputeShader bezierCS;
 
-		private Transform[] points;
-		private CommandBuffer cmd;
-		private Camera mainCamera;
 
+		private Camera mainCamera;
+		private Vector3[] points;
+
+		private CommandBuffer cmd;
 		private RenderTexture screenRT;
+		private ComputeBuffer inputPoints_cb, outPoints_cb;
 
 
 		private void OnEnable()
 		{
-			mainCamera = Camera.main;
-			points = pointsParent.Cast<Transform>().ToArray();
-
 			InitCmd();
+			
+			if (cmd != null)
+			{
+				Graphics.ExecuteCommandBuffer(cmd);
+				Vector3[] ps = new Vector3[tCount];
+				outPoints_cb.GetData(ps);
+				foreach (var item in ps)
+				{
+					Debug.Log(item);
+				}
+			}
 		}
 
 		private void OnDisable()
@@ -41,18 +51,22 @@ namespace Games_101_4
 			cmd?.Dispose();
 			cmd = null;
 			Destroy(screenRT);
+			inputPoints_cb?.Dispose();
+			inputPoints_cb = null;
+			outPoints_cb?.Dispose();
+			outPoints_cb = null;
 		}
 
 		private void Update()
 		{
-			if (cmd != null)
-			{
-				Graphics.ExecuteCommandBuffer(cmd);
-			}
+
 		}
 
 		private void InitCmd()
 		{
+			mainCamera = Camera.main;
+			points = pointsParent.Cast<Transform>().Select(x => x.position).ToArray();
+
 			int width = mainCamera.scaledPixelWidth;
 			int height = mainCamera.scaledPixelHeight;
 			screenRT = new RenderTexture(width, height, 0, GraphicsFormat.R8G8B8A8_UNorm)
@@ -60,16 +74,32 @@ namespace Games_101_4
 				name = "MyScreenRT",
 				enableRandomWrite = true,
 			};
-			
+
 			GetComponent<BlitMonoCtrl>().rt = screenRT;
 
-			int mainKernel = bezierCS.FindKernel(k_CSMain);
+			if (tCount < 2)
+			{
+				return;
+			}
+
+			int calcPoints_kernel = bezierCS.FindKernel(k_CalcPoints);
+			bezierCS.SetInt("_PointsCount", points.Length);
+			bezierCS.SetFloat("_TStep", 1.0f / (tCount - 1));
+			inputPoints_cb = new ComputeBuffer(points.Length, 3 * sizeof(float), ComputeBufferType.Structured);
+			outPoints_cb = new ComputeBuffer(tCount, 3 * sizeof(float), ComputeBufferType.Structured);
+			inputPoints_cb.SetData(points);
+			bezierCS.SetBuffer(calcPoints_kernel, "_Points", inputPoints_cb);
+			bezierCS.SetBuffer(calcPoints_kernel, "_BezierPoints", outPoints_cb);
+
+
+			int blitScreen_kernel = bezierCS.FindKernel(k_BlitScreen);
 			bezierCS.SetInt(Width_ID, width);
 			bezierCS.SetInt(Height_ID, height);
-			bezierCS.SetTexture(mainKernel, Result_ID, screenRT);
+			bezierCS.SetTexture(blitScreen_kernel, Result_ID, screenRT);
 
 			cmd = new CommandBuffer {name = "Bezier"};
-			cmd.DispatchCompute(bezierCS, mainKernel, Mathf.CeilToInt(width / 8.0f), Mathf.CeilToInt(height / 8.0f), 1);
+			cmd.DispatchCompute(bezierCS, calcPoints_kernel, tCount, 1, 1);
+			cmd.DispatchCompute(bezierCS, blitScreen_kernel, Mathf.CeilToInt(width / 8.0f), Mathf.CeilToInt(height / 8.0f), 1);
 		}
 	}
 }

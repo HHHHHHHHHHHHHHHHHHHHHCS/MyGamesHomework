c247cebc0f7f98ee7782c8ad931f09f71ca3604c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-03-12 15:53:52 +0800

    add

---
+++ Assets/Games_103_1/Rigid_Bunny.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Games_103_1
+{
+	public class Rigid_Bunny : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+		}
+	}
+}
